900f022a00554c5a0035f65d7341997f725e6804
Victor Grigoriu <vgrigoriu@gmail.com>
2017-02-06 20:55:32 +0200

    Add Rank enum

---
+++ Rank.cs
@@ -0,0 +1,19 @@
+namespace Whist
+{
+    public enum Rank
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+    }
+}
