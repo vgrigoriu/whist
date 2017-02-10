8fa7689d7f46fb96297bc32a8ecec125a99a91aa
Victor Grigoriu <vgrigoriu@gmail.com>
2017-02-10 22:54:48 +0200

    GetValidFollowUps takes current trump

--- Whist/Tests/GetValidFollowUpsTests.cs
+++ Whist/Tests/GetValidFollowUpsTests.cs
@@ -6,7 +6,7 @@ namespace Tests
     public class GetValidFollowUpsTests
     {
         [Fact]
-        public void ReturnsAllCardsOfTheSameSuit()
+        public void ReturnsAllCardsOfTheSameSuitWhenNoTrump()
         {
             var initialCard = new Card(Rank.Ace, Suit.Spade);
             var hand = new[]
@@ -21,7 +21,34 @@ namespace Tests
                 new Card(Rank.Five, Suit.Spade),
             };
 
-            var validCards = Rules.GetValidFollowUps(initialCard, hand);
+            var validCards = Rules.GetValidFollowUps(initialCard, null, hand);
+
+            Assert.Equal(
+                new[]
+                {
+                    new Card(Rank.Two, Suit.Spade),
+                    new Card(Rank.Five, Suit.Spade),
+                },
+                validCards);
+        }
+
+        [Fact]
+        public void ReturnsAllTrumpCardsWhenInitialCardMatchesTrump()
+        {
+            var initialCard = new Card(Rank.Ace, Suit.Spade);
+            var hand = new[]
+            {
+                new Card(Rank.Two, Suit.Club),
+                new Card(Rank.Two, Suit.Diamond),
+                new Card(Rank.Two, Suit.Heart),
+                new Card(Rank.Two, Suit.Spade),
+                new Card(Rank.Five, Suit.Club),
+                new Card(Rank.Five, Suit.Diamond),
+                new Card(Rank.Five, Suit.Heart),
+                new Card(Rank.Five, Suit.Spade),
+            };
+
+            var validCards = Rules.GetValidFollowUps(initialCard, Suit.Spade, hand);
 
             Assert.Equal(
                 new[]
--- Whist/WhistCore/Rules.cs
+++ Whist/WhistCore/Rules.cs
@@ -5,7 +5,10 @@ namespace WhistCore
 {
     public static class Rules
     {
-        public static IEnumerable<Card> GetValidFollowUps(Card initialCard, IEnumerable<Card> hand)
+        public static IEnumerable<Card> GetValidFollowUps(
+            Card initialCard,
+            Suit? trump,
+            IEnumerable<Card> hand)
         {
             return hand.Where(card => card.Suit == initialCard.Suit);
         }
