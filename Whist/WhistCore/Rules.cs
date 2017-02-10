ec3fbb25ecbe11f6bd21c721bf0cac6870f37008
Victor Grigoriu <vgrigoriu@gmail.com>
2017-02-10 22:56:41 +0200

    Add comment to rules class

--- Whist/WhistCore/Rules.cs
+++ Whist/WhistCore/Rules.cs
@@ -5,6 +5,15 @@ namespace WhistCore
 {
     public static class Rules
     {
+        /// <summary>
+        /// The other players must play a card of the same suit if possible.
+        /// Any player who has no card of the suit led must play a trump if they can.
+        /// A player who has no cards of the suit led and no trumps can discard any card.
+        /// </summary>
+        /// <param name="initialCard"></param>
+        /// <param name="trump"></param>
+        /// <param name="hand"></param>
+        /// <returns></returns>
         public static IEnumerable<Card> GetValidFollowUps(
             Card initialCard,
             Suit? trump,
